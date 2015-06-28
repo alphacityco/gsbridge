f1e8e6ed45453268d9ec36ffda852651e0d8ea89
Jose Pino <pino.jose@gmail.com>
2015-06-28 18:52:54 -0300

    init commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.post('/spreasheet', function (req, res) {
+
+  res.send('Hello World!');
+});
+
+  var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Google Spreadsheet Bridge app listening at http://%s:%s', host, port);
+});
